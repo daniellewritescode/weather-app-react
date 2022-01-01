b72e77b340d0c613307f5980a9e93e0d1bb7bd1f
daniellewritescode <95192302+daniellewritescode@users.noreply.github.com>
2022-01-01 10:22:12 -0500

    Current Time Component
    removed hard coding and added code to calculate the current time.

--- src/CurrentTime.js
+++ src/CurrentTime.js
@@ -2,5 +2,17 @@ import React from "react";
 import "./style.css";
 
 export default function CurrentTime(time) {
-  return <div>Friday, December 31, 2021 14:24PM ET</div>;
+let monthsofYear = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+let daysofWeek = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let now = new Date();
+let currentMonth = monthsofYear[now.getMonth()];
+let currentDay = daysofWeek[now.getDay()];
+let currentHours = now.getHours();
+let currentMinutes = now.getMinutes();
+let currentDate = now.getDate();
+let currentYear = now.getFullYear();
+if (currentMinutes < 10) {currentMinutes = `0${currentMinutes}`;}
+let currentTimeCalc = (`${currentDay}, ${currentMonth} ${currentDate}, ${currentYear} ${currentHours}:${currentMinutes} ET`);
+
+  return <div>{currentTimeCalc}</div>;
 }
